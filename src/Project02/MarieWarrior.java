74d06cc6f8d81772f7eadd1f298c9408d97cea91
Marie Stella <addom@moravian.edu>
2023-03-07 10:50:07 -0500

    created new classes

---
+++ src/Project02/MarieWarrior.java
@@ -0,0 +1,12 @@
+package Project02;
+
+public class MarieWarrior extends People{
+    MarieWarrior(String nation, String tribe, int lifePoints, Strategy MarieWarriorStrategy) {
+        super(nation, tribe, PeopleType.warrior, lifePoints, MarieWarriorStrategy);
+        myDescription = "\tMarie Warrior";
+    }
+    @Override
+    public int encounterLifePoints(People me, People otherPerson) {
+        return 0;
+    }
+}
