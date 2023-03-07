3bc577aa5b4d18ddd70e7445b0e358e26091813d
JeremyGoll <GollJ@moravian.edu>
2023-03-07 10:58:17 -0500

    added Warrior and Wizard mathods

--- src/Project02/JeremyWarrior.java
+++ src/Project02/JeremyWarrior.java
@@ -1,4 +1,16 @@
 package Project02;
 
-public class JeremyWarrior {
+public class JeremyWarrior extends People{
+
+    JeremyWarrior(String nation, String tribe, int lifePoints, Strategy MarieWarriorStrategy)
+    {
+        super(nation, tribe, PeopleType.warrior, lifePoints, MarieWarriorStrategy);
+        myDescription = "\tMarie Warrior";
+    }
+
+    @Override
+    public int encounterLifePoints(People me, People otherPerson)
+    {
+        return 0;
+    }
 }
--- src/Project02/JeremyWizard.java
+++ src/Project02/JeremyWizard.java
@@ -1,4 +1,18 @@
 package Project02;
 
-public class JeremyWizard {
+public class JeremyWizard extends People
+{
+    JeremyWizard(String nation, String tribe, int lifePoints, Strategy strategy)
+    {
+        super(nation, tribe, PeopleType.wizard, lifePoints, strategy);
+        myDescription = "\tJeremy Wizard";
+
+    }
+
+    @Override
+    public int encounterLifePoints(People me, People otherPerson)
+    {
+        return encounterStrategy.strategy(me, otherPerson);
+    }
+
 }
