83a8bdd86e20d58364c61a03895bfc240aacf3cf
JeremyGoll <GollJ@moravian.edu>
2023-03-20 20:51:49 -0400

    Added tests for JeremysWizardStart

--- test/NateWarriorTests.java
+++ test/NateWarriorTests.java
@@ -2,7 +2,7 @@ import Project02.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class NateTests
+class NateTests
 {
     @Test
     public void testNateWarriorStrategyAgainstSameNationWarrior()
