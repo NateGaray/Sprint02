7f70f49fa90e977f6a84fc37aacd05531c37a7f1
Nathan <garayn@moravian.edu>
2023-03-21 11:15:04 -0400

    Finished Tests

--- tests/NateWizardTests.java
+++ tests/NateWizardTests.java
@@ -17,4 +17,26 @@ public class NateWizardTests
         assertEquals(110, Yinx.getLifePoints());
         assertEquals(90, nateWizardStrategy.strategy(Harry, Yinx));
     }
+
+    @Test
+    public void testNotSameNationAndLifePointsGreaterThanOppoment()
+    {
+        NateWizardStrategy nateWizardStrategy = new NateWizardStrategy();
+        NateWarriorStrategy nateWarriorStrategy = new NateWarriorStrategy();
+        People Harry = new NateWizard("Nate", "0", 100, nateWizardStrategy);
+        People Yinx = new NateWarrior("Bink", "0", 90, nateWarriorStrategy);
+
+        assertEquals(90, nateWizardStrategy.strategy(Harry, Yinx));
+    }
+
+    @Test
+    public void testNotSameNationAndLifePointsLessThan()
+    {
+        NateWizardStrategy nateWizardStrategy = new NateWizardStrategy();
+        NateWarriorStrategy nateWarriorStrategy = new NateWarriorStrategy();
+        People Harry = new NateWizard("Nate", "0", 90, nateWizardStrategy);
+        People Yinx = new NateWarrior("Bink", "0", 100, nateWarriorStrategy);
+
+        assertEquals(80, nateWizardStrategy.strategy(Harry, Yinx));
+    }
 }
