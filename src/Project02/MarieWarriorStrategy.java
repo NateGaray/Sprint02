2190776c9e54a5227d47c2430b61e11fdc587c13
Marie Stella <addom@moravian.edu>
2023-03-28 10:46:22 -0400

    Marie Warrior Strategy And Tests

--- src/Project02/MarieWarriorStrategy.java
+++ src/Project02/MarieWarriorStrategy.java
@@ -5,28 +5,28 @@ package Project02;
  * This strategy will run a defensive strategy during the battle standoff.
  * When the warrior faces of against an opponent that is weaker it will attack at a great force
  * to try to impact the most damage to that opponent.
+ * if the opponent is a warrior with lower points, me warrior will attack by taking half of the other warrior points
  * 2.
  * When the warrior faces of against an opponent with the same number of life points, it will work to maker sure
  * it also makes the most impact in that meet.
+ * 3.
  * If it meets a stronger opponent, the play will be to engage in the safest way while trying to save guard the
  * points that the warrior has a that time.
- * 3.
+ * 4.
  *
  */
 public class MarieWarriorStrategy implements Strategy
 {
         @Override
         public int strategy(People me, People otherPerson) {
-        int lifePoints = 0;
-        if (me.getNation() != otherPerson.getNation()){
-            if (otherPerson.getLifePoints() < me.getLifePoints()){
-                if (me.getType() == PeopleType.warrior){
-                    lifePoints = -otherPerson.getLifePoints(); //decimate the opponent
-                }
-                else {
-                    if (me.getType() == PeopleType.wizard){
-                        lifePoints = me.getLifePoints()/2;
-                    }
+        int lifePoints = me.getLifePoints();
+        if (otherPerson.getLifePoints() < me.getLifePoints()){ //other less points than me
+            if (me.getType() == PeopleType.warrior && otherPerson.getType() == PeopleType.wizard){ //me is warrior
+                lifePoints = 0; //decimate the opponent
+            }
+            if (me.getLifePoints() == otherPerson.getLifePoints()){
+                if (otherPerson.getType() == PeopleType.warrior && me.getType() == PeopleType.warrior){
+                    lifePoints = otherPerson.getLifePoints()/2;
                 }
             }
         }
