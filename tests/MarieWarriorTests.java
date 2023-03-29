75f6d5ad9e9a6a6a2658b92ae79dcc36d46708e9
Marie Stella <addom@moravian.edu>
2023-03-29 13:49:44 -0400

    Final tests

--- tests/MarieWarriorTests.java
+++ tests/MarieWarriorTests.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class MarieWarriorTests {
+import Project02.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MarieWarriorTests {
+    @Test
+    public void testMyWarriorIsAlwaysOnGuardOtherIsNotWarrior(){
+        MarieWarriorStrategy marieWarriorStrategy = new MarieWarriorStrategy();
+        People people = new MarieWarrior("Minions", "3",100,marieWarriorStrategy);
+        People other = new MarieWizard("Idiots", "2", 99, marieWarriorStrategy);
+
+        assertEquals(0,marieWarriorStrategy.strategy(people, other));
+    }
+
+    @Test
+    public  void testMyWarriorOtherWizardHasLessPoints(){
+        MarieWarriorStrategy marieWarriorStrategy = new MarieWarriorStrategy();
+        People mePeople = new MarieWarrior("Idiots", "2", 100,marieWarriorStrategy);
+        People other = new MarieWizard("Minions", "1", 98, marieWarriorStrategy);
+        assertEquals(77,marieWarriorStrategy.strategy(mePeople,other));
+    }
+
+    @Test
+    public void testOtherWarriorAlsoAndPointsLessThanMeWarrior(){
+        MarieWarriorStrategy marieWarriorStrategy = new MarieWarriorStrategy();
+        People meWarrior = new MarieWarrior("Idiots","2",98,marieWarriorStrategy);
+        People otherWarrior = new MarieWarrior("Minions", "3", 97,marieWarriorStrategy);
+        assertEquals(72,marieWarriorStrategy.strategy(meWarrior,otherWarrior));
+    }
+
+    @Test
+    public void testOtherWarriorAlsoButPointsEqualToMeWarrior(){
+        MarieWarriorStrategy marieWarriorStrategy = new MarieWarriorStrategy();
+        People meWarrior = new MarieWarrior("Idiots","2",55,marieWarriorStrategy);
+        People otherWarrior = new MarieWarrior("Minions", "3",55,marieWarriorStrategy);
+        assertEquals(25,marieWarriorStrategy.strategy(meWarrior,otherWarrior));
+    }
+
+    @Test
+    public void testIfBothWarriorButOtherPointsGreaterThanMe(){
+        MarieWarriorStrategy marieWarriorStrategy = new MarieWarriorStrategy();
+        People meWarrior = new MarieWarrior("Idiots", "2", 56,marieWarriorStrategy);
+        People otherWarrior = new MarieWarrior("Minions", "3", 65, marieWarriorStrategy);
+        assertEquals(46, marieWarriorStrategy.strategy(meWarrior,otherWarrior));
+    }
+
+    @Test
+    public void testIfMeWizardAndOtherWarrior(){
+        MarieWarriorStrategy marieWarriorStrategy = new MarieWarriorStrategy();
+        People meWizard = new MarieWizard("Idiots", "2",56,marieWarriorStrategy);
+        People otherWarrior = new MarieWarrior("Minions", "3",65,marieWarriorStrategy);
+        assertEquals(36,marieWarriorStrategy.strategy(meWizard,otherWarrior));
+    }
 }
--- tests/MarieWizardTests.java
+++ tests/MarieWizardTests.java
@@ -1,54 +1,34 @@
 import Project02.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-public class MarieWarriorTests
+public class MarieWizardTests
 {
-//@Test
-//    public void testDifferentNationDifferentTribeDifferentPeople()
-//    {
-//    MarieWarriorStrategy marieWarriorStrategy = new MarieWarriorStrategy();
-//
-//    People England = new MarieWizard("Idiot","1",100,marieWarriorStrategy);
-//    People Russia = new MarieWizard("Minion","2",100,marieWarriorStrategy);
-//    marieWarriorStrategy.strategy(England,Russia);
-//    assertEquals(100,marieWarriorStrategy.strategy(Russia,England));
-//    }
 @Test
-    public void testDifferentNationDifferentTribeDifferentPeopleLessLifePointsThanMe(){
+
+    public void testDifferentNationDifferentTribeDifferentPeople()
+    {
+    MarieWarriorStrategy marieWarriorStrategy = new MarieWarriorStrategy();
+
+    People England = new MarieWizard("Idiot","1",100,marieWarriorStrategy);
+    People Russia = new MarieWizard("Minion","2",100,marieWarriorStrategy);
+    marieWarriorStrategy.strategy(England,Russia);
+    assertEquals(100,marieWarriorStrategy.strategy(Russia,England));
+    }
+@Test
+    public void testDecimateOpponentIfPointsLessThan10AndTribeAndNationDifferent(){
     MarieWizardStrategy marieWizardStrategy1 = new MarieWizardStrategy();
     MarieWizardStrategy marieWizardStrategy2 = new MarieWizardStrategy();
     People England = new MarieWizard("Idiot", "1", 100, marieWizardStrategy1);
-    People Russia = new MarieWizard("Minions", "2", 98, marieWizardStrategy2);
-    assertEquals(50, marieWizardStrategy1.strategy(England,Russia));
-    assertEquals(0,marieWizardStrategy2.strategy(Russia, England));
-    }
-
-
-    @Test
-    public void testDecimateOpponentIfPointsLessThan10(){
-    MarieWizardStrategy marieWizardStrategy = new MarieWizardStrategy();
-    People England = new MarieWizard("Idiot", "2",100,marieWizardStrategy);
-    People Russia = new MarieWizard("Minions", "2",9,marieWizardStrategy);
-    assertEquals(0,marieWizardStrategy.strategy(England, Russia));
+    People Russia = new MarieWizard("Minions", "2", 8, marieWizardStrategy2);
+    assertEquals(0,marieWizardStrategy2.strategy(England, Russia));
     }
 
     @Test
-    public void testSubtract5FromOpponentIfPointsLessThanMine(){
+    public void testIfMePointsLessThanOtherLifePointsNationsNotEqualOtherWarriorMeWizard(){
     MarieWizardStrategy marieWizardStrategy = new MarieWizardStrategy();
-    People England = new MarieWizard("Idiot", "3", 55,marieWizardStrategy);
-    People Russia = new MarieWizard("Minions", "3", 54, marieWizardStrategy);
-    assertEquals(49,marieWizardStrategy.strategy(England,Russia));
+    People England = new MarieWizard("Idiot", "2",9,marieWizardStrategy);
+    People Russia = new MarieWarrior("Minions", "2",100,marieWizardStrategy);
+    assertEquals(3,marieWizardStrategy.strategy(England, Russia));
     }
-
-    @Test
-    public void testSpecificPeopleTypeAndOtherPointsGreaterThanMePoints(){
-    MarieWizardStrategy marieWizardStrategy = new MarieWizardStrategy();
-   People meWizard = new MarieWarrior("Idiot","5",25,marieWizardStrategy);
-   People otherWarrior = new MarieWarrior("Minions", "3", 35,marieWizardStrategy);
-   if (otherWarrior.getType()== PeopleType.warrior)
-       int lifePoints = meWizard.modifyLi; / 2;
-
-    }
-
 }
 
