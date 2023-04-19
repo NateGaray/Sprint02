f6a89222eb09ed40c6688646521f923260540eaa
Nathan <garayn@moravian.edu>
2023-04-19 12:57:25 -0400

    Finished warrior tests

--- tests/NateWarriorTests.java
+++ tests/NateWarriorTests.java
@@ -14,4 +14,30 @@ public class NateWarriorTests
         assertEquals(39, nateWarriorStrategy.strategy(Franq, Josh));
 
     }
+
+    @Test
+    public void testTakesMediumDamage()
+    {
+        NateWarriorStrategy nateWarriorStrategy = new NateWarriorStrategy();
+        People Franq = new NateWarrior("Nate", "0", 65, nateWarriorStrategy);
+        People Josh = new NateWarrior("Bee", "0", 89, nateWarriorStrategy);
+
+        assertEquals(52, nateWarriorStrategy.strategy(Franq, Josh));
+
+        People Franqlynn = new NateWarrior("Nate", "0", 60, nateWarriorStrategy);
+        People Joshmin = new NateWarrior("Bee", "0", 33, nateWarriorStrategy);
+
+        assertEquals(48, nateWarriorStrategy.strategy(Franqlynn, Joshmin));
+
+    }
+
+    @Test
+    public void testTakesLightDamage()
+    {
+        NateWarriorStrategy nateWarriorStrategy = new NateWarriorStrategy();
+        People Franq = new NateWarrior("Nate", "0", 45, nateWarriorStrategy);
+        People Josh = new NateWarrior("Bee", "0", 69, nateWarriorStrategy);
+
+        assertEquals(41, nateWarriorStrategy.strategy(Franq, Josh));
+    }
 }
