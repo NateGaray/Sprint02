84d6ff6364b93b7266fe495402d08688be500c41
Nathan <garayn@moravian.edu>
2023-04-26 12:52:39 -0400

    Modified artifact strategy

--- src/Project02/NateArtifactStrategy.java
+++ src/Project02/NateArtifactStrategy.java
@@ -1,7 +1,8 @@
 package Project02;
 
 /**
- * This class defines the strategy for a witch;
+ * This class defines the strategy for a witch
+ * The witch trows potions that can either cause damage, heal, or do nothing
  */
 
 public class NateArtifactStrategy implements Strategy
@@ -17,20 +18,28 @@ public class NateArtifactStrategy implements Strategy
     public int strategy(People me, People otherPerson)
     {
         Die die = new Die();
-        die.setDie(30);
+        die.setDie(40);
         die.rollDie();
 
-        if (die.getDie() % 3 == 0)
+        if (die.getDie() % 11 == 0)
         {
-           return otherPerson.getLifePoints() - 5;
+           return otherPerson.getLifePoints(); // Decoy potion. No damage dealt.
         }
-        else if (die.getDie() % 3 == 1)
+        else if (die.getDie() % 4 == 0)
         {
-            return otherPerson.getLifePoints() - 12;
+            return otherPerson.getLifePoints() - 5; // Damage potion
+        }
+        else if (die.getDie() % 4 == 1)
+        {
+            return otherPerson.getLifePoints() + 5; // Healing potion
+        }
+        else if (die.getDie() % 4 == 2)
+        {
+            return otherPerson.getLifePoints() - 12; // Damage potion
         }
         else
         {
-            return otherPerson.getLifePoints() - 21;
+            return otherPerson.getLifePoints() + 12; // Healing potion
         }
     }
 }
